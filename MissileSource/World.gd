f22d29131931c9265916c718f3ef0af51d3249b4
chickaloo <chrisjhkong@gmail.com>
2019-03-16 22:58:00 -0700

    added functional enemy spawns and dying

--- MissileSource/World.gd
+++ MissileSource/World.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 var _PLAYER = preload("res://objects/player/classic/Player.tscn")
+var _ENEMY = preload("res://objects/enemy/classic/EnemyClassic.tscn")
+var timer = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,8 +17,12 @@ func _ready():
 	
 	pass # Replace with function body.
 
-
-#func _process(delta):
-#	SPAWN ENEMIES @ RANDOM 
-
-#	pass
+func _process(delta):
+	if timer%100 == 0:
+		var enemy = _ENEMY.instance()
+		enemy.global_position = Vector2( randi()%int(get_viewport().size.x), -32 )
+		add_child(enemy)
+		enemy.set_variables(Vector2(randi()%int(get_viewport().size.x), get_viewport().size.y))
+		
+	timer = timer + 1
+	pass
--- MissileSource/objects/enemy/classic/EnemyClassic.gd
+++ MissileSource/objects/enemy/classic/EnemyClassic.gd
@@ -1,13 +1,32 @@
 extends Area2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var DIRECTION = Vector2(0,0)
+var DESTINATION = Vector2(0,0)
+var SPEED = 300
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func set_variables(dest):
+	DESTINATION = dest
+	var DIR = DESTINATION-self.global_position
+	var dist = self.global_position.distance_to(DESTINATION)
+	DIRECTION = Vector2( DIR.x/dist, DIR.y/dist )
+	
+	pass
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	
+	if self.global_position.distance_to(DESTINATION) < 5:
+		#reduce city to rubble
+		queue_free()
+		
+	var collisions = self.get_overlapping_areas()
+	if collisions.size() != 0:
+		queue_free()
+		
+	self.global_position = self.global_position + (DIRECTION * SPEED * delta)
+	
+	pass
